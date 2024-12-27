e0a5ce74e91e628639e29b9f19aeded928d4cddf
thevu1000 <thevu7000@gmail.com>
2024-12-27 17:44:42 +0700

    Remove Vercel json

--- server.js
+++
@@ -1,9 +0,0 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Đường dẫn tới file db.json
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-
-module.exports = server;
